9728bbd07eb297344aca913e6367c486be5897ad
Chris <13564494+cmartin300@users.noreply.github.com>
2025-09-29 21:19:54 -0700

    Add core gameplay loop
    setup rng for computer choice
    setup prompt for user choice
    handle errors with input or rng
    handle win conditions

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Hello, world!");
+console.log("Rock, Paper, Scissors!");
+
+// get computer choice
+let rng = Math.floor(Math.random() * 3);
+let computerChoice =
+  rng === 0 ? "Rock" : rng === 1 ? "Paper" : rng === 2 ? "Scissors" : "Err";
+
+if (computerChoice === "Err") {
+  console.log(`Error in computer choice. rng = ${rng}`);
+}
+
+// prompt for user choice
+let userSelection = prompt(
+  "Choose: Rock {R}, Paper{P}, or Scissors{S}: "
+).toUpperCase();
+
+let userChoice =
+  userSelection === "R"
+    ? "Rock"
+    : userSelection === "P"
+    ? "Paper"
+    : userSelection === "S"
+    ? "Scissors"
+    : "Err";
+
+if (userChoice === "Err") {
+  console.log(
+    `Error in user choice: userSelection is ${userSelection}. Needs to be R, P, or S!`
+  );
+}
+
+// check win condition
+console.log(`Computer chose: ${computerChoice}.\nYou chose: ${userChoice}.`);
+if (computerChoice === userChoice) {
+  console.log("It's a draw!");
+} else if (
+  (computerChoice === "Rock" && userChoice === "Scissors") ||
+  (computerChoice === "Paper" && userChoice === "Rock") ||
+  (computerChoice === "Scissors" && userChoice === "Paper")
+) {
+  console.log("Computer Wins!");
+} else {
+  console.log("You win!");
+}
